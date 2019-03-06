fe10e143f7ac9d23c6ed2fcf2bae0d6e6f95ee7c
Matt Crowder <mcrowder65@gmail.com>
2019-03-05 20:05:19 -0500

    right before hooks refactor

--- src/app.js
+++ src/app.js
@@ -5,18 +5,24 @@ import { createBrowserHistory } from "history";
 import OneHoc from "./one-hoc";
 import { SnackbarProvider } from "./snackbar";
 import OneRenderProps from "./one-renderprops";
-
+import LoadingProvider from "./loading";
+import TwoHoc from "./two-hoc";
+import TwoRenderProps from "./two-renderprops";
 const browserHistory = createBrowserHistory();
 class App extends Component {
   render() {
     return (
       <SnackbarProvider>
-        <BrowserRouter history={browserHistory}>
-          <div>
-            <Route exact path="/one-hoc" component={OneHoc} />
-            <Route path="/one-renderprops" component={OneRenderProps} />
-          </div>
-        </BrowserRouter>
+        <LoadingProvider>
+          <BrowserRouter history={browserHistory}>
+            <div>
+              <Route exact path="/one-hoc" component={OneHoc} />
+              <Route path="/one-renderprops" component={OneRenderProps} />
+              <Route path="/two-hoc" component={TwoHoc} />
+              <Route path="/two-renderprops" component={TwoRenderProps} />
+            </div>
+          </BrowserRouter>
+        </LoadingProvider>
       </SnackbarProvider>
     );
   }
